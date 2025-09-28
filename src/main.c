b0c1efd465cd78e063f2475b41ff8c509079806e
Jarl Evanson <evanson.jarl@gmail.com>
2025-09-28 16:39:32 -0500

    Integrate `Parser` into `main.c` and `fuzz.c`

--- src/main.c
+++ src/main.c
@@ -3,9 +3,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char* argv[]) {
-    if (argc == 1) {
+#include "parser.h"
+#include "vm.h"
 
+void drive(Vm* vm, Parser* parser) {
+    ParseResult parse_result;
+    while (parser_next_sexpr(vm, parser, &parse_result)) {
+        if (!parse_result.ok) {
+            parse_context_print(parse_result.as.err, parser);
+            continue;
+        }
+
+        PRINT_SEXPR(parse_result.as.ok);
+        printf("\n");
+    }
+}
+
+int main(int argc, char* argv[]) {
+    Vm vm;
+    Parser parser;
+
+    if (argc == 1) {
+        if (!vm_init(&vm)) {
+            fprintf(stderr, "failed to initialize VM\n");
+            return EXIT_FAILURE;
+        }
+        parser_init(&parser, stdin, stdout);
+
+        drive(&vm, &parser);
+
+        parser_free(&parser);
+        vm_free(&vm);
     } else if (argc == 2) {
         FILE* file = fopen(argv[1], "rb");
         if (file == NULL) {
@@ -18,7 +46,17 @@ int main(int argc, char* argv[]) {
             return EXIT_FAILURE;
         }
 
+        if (!vm_init(&vm)) {
+            fprintf(stderr, "failed to initialize VM\n");
+            return EXIT_FAILURE;
+        }
+        parser_init(&parser, file, NULL);
+
+        drive(&vm, &parser);
+
         fclose(file);
+        parser_free(&parser);
+        vm_free(&vm);
     } else {
         fprintf(stderr, "usage: lisp [path]\n");
         return EXIT_FAILURE;
