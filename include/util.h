bb694106dc2fd5673cf380765666a1f7d286ec87
Jarl Evanson <evanson.jarl@gmail.com>
2025-10-01 14:24:40 -0500

    Add memory manipulation functions to `util.h`

--- include/util.h
+++ include/util.h
@@ -4,10 +4,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "common.h"
+
 /// Returns the number of objects in the array.
 #define countof(obj) ((size_t) (sizeof(obj) / sizeof(*(obj))))
 /// Returns the alignment of the given type.
 #define alignof(type) offsetof(struct { uint8_t c; type d; }, d)
+
+static inline bool valid_capacity(size_t capacity, size_t size) {
+    return capacity <= PTRDIFF_MAX / size;
+}
+
+static inline bool grow_capacity(size_t* capacity, size_t size, size_t base_capacity) {
+    size_t new_capacity = *capacity == 0 ? base_capacity : *capacity * 2;
+    if (new_capacity < *capacity || !valid_capacity(new_capacity, size)) {
+        new_capacity = PTRDIFF_MAX / size;
+    }
+
+    if (new_capacity <= *capacity) {
+        return false;
+    }
+
+    *capacity = new_capacity;
+    return true;
+}
+
+static inline bool grow(void** ptr, size_t* capacity, size_t size, size_t base_capacity) {
+    size_t tmp_capacity = *capacity;
+    if (!grow_capacity(&tmp_capacity, size, base_capacity)) return false;
+
+    size_t alloc_size = tmp_capacity * size;
+    void* new_ptr = realloc(*ptr, alloc_size);
+    if (new_ptr == NULL) {
+        return false;
+    }
+
+    *capacity = tmp_capacity;
+    *ptr = new_ptr;
+    return true;
+}
+
+static inline bool is_power_of_two(uintmax_t val) {
+    if (val == 0) return true;
+
+    uintmax_t val_0 = val & (val - 1);
+    if (val_0 == 0) return true;
+
+    return false;
+}
+
+static inline uintmax_t floor_power_of_two(uintmax_t val) {
+    while ((val & (val - 1)) != 0) {
+        val &= (val - 1);
+    }
+
+    return val;
+}
+
+#define GROW(ptr, capacity, size, base_capacity) \
+    grow((void**) (ptr), (capacity), (size), (base_capacity))
 
 #define ASSERT(assertion, ...) do { \
     if ((assertion)) break; \
